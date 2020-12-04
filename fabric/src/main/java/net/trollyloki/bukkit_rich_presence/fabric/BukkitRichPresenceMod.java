a57ff968f0a08d07378aefc8917e54854dfa00e2
TrollyLoki <trollyloki@factmc.net>
2020-12-04 13:20:45 -0500

    Bump version

--- fabric/src/main/java/net/trollyloki/bukkit_rich_presence/fabric/BukkitRichPresenceMod.java
+++ fabric/src/main/java/net/trollyloki/bukkit_rich_presence/fabric/BukkitRichPresenceMod.java
@@ -8,7 +8,7 @@ import net.fabricmc.api.ClientModInitializer;
 public class BukkitRichPresenceMod implements ClientModInitializer {
 
 	public static final Logger LOGGER = LogManager.getLogger();
-	public static final String VERSION = "0.1";
+	public static final String VERSION = "0.1.1";
 
 	public static BukkitRichPresenceMod instance;
 
